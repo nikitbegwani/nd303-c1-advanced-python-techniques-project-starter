a943fac522789cd682be1fc6c3582b910b49cbf1
Nikit Begwani <nibegwan@microsoft.com>
2020-03-01 12:07:41 +0530

    implemented first solution for load_data and NEODatabase initializer

--- starter/database.py
+++ starter/database.py
@@ -1,4 +1,5 @@
 from models import OrbitPath, NearEarthObject
+import csv
 
 
 class NEODatabase(object):
@@ -17,6 +18,12 @@ class NEODatabase(object):
         # TODO: What data structures will be needed to store the NearEarthObjects and OrbitPaths?
         # TODO: Add relevant instance variables for this.
 
+        self.filename = filename
+        
+        self.obj_name = {}
+        self.obj.date = {}
+
+
     def load_data(self, filename=None):
         """
         Loads data from a .csv file, instantiating Near Earth Objects and their OrbitPaths by:
@@ -33,6 +40,25 @@ class NEODatabase(object):
         filename = filename or self.filename
 
         # TODO: Load data from csv file.
+
         # TODO: Where will the data be stored?
+
+        fp_open = open(filename, 'r')
+
+        obj_data = csv.DictReader(fp_open)
+
+        for entry in obj_data:
+            orbit_path = OrbitPath(**entry)
+
+            if self.obj_name.get(entry['name'], None) is None:
+                self.obj_name[entry['name']] = NearEarthObject(**entry)
+
+            if self.obj_date.get(entry['close_approach_date'], None) is None:
+                self.obj_date[entry['close_approach_date']] = []
+
+            neo = self.obj_name.get(entry['name'], None)            
+            neo.update_orbits(orbit_path)
+
+            self.obj_date[entry['close_approach_date']].append(neo)
 
         return None
